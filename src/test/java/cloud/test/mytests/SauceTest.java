6ceea0162167ff9bdb8e57c6ef9acbe7bf23f3b8
alisha <alishazia23.05@gmail.com>
2022-05-08 15:19:05 +0530

    Initial Commit - working code for Sauce lab

---
+++ src/test/java/cloud/test/mytests/SauceTest.java
@@ -0,0 +1,30 @@
+package cloud.test.mytests;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class SauceTest extends BaseTest
+{
+    public void doLogin()
+    {
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+    }
+
+    @Test(priority=1)
+    public void checkInventoryItemTest()
+    {
+        doLogin();
+        Assert.assertTrue(driver.findElements(By.cssSelector(".inventory_item")).size() == 6);
+    }
+
+    @Test(priority=2)
+    public void addToCartButtonTest()
+    {
+        doLogin();
+        Assert.assertTrue(driver.findElements(By.xpath("//button[text()='ADD TO CART']")).size() == 6);
+    }
+}
